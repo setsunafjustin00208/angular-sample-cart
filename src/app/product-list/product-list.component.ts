0cb14a4128b58adfb0419691487c37dbcf8d9f21
setsunafjustin00208 <102853563+setsunafjustin00208@users.noreply.github.com>
2023-07-19 17:19:53 +0800

    3 files modified, 8 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = [...products];
@@ -13,8 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
